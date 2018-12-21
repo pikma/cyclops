9c886fc29b02e3a827baedd96bb990184f92892b
Pierre Kreitmann <kreitmann@google.com>
2018-12-21 11:38:59 +0100

    Refactoring.
    Simpler state representation.

--- main.py
+++ main.py
@@ -11,21 +11,20 @@ Computing Strategies in Two-Player Simultaneous Move Games', by Bosansky et
 al (http://mlanctot.info/files/papers/aij-2psimmove.pdf).
 '''
 
-
 import collections
 
 import numpy as np
 from ortools.linear_solver import pywraplp
 
-NUM_COINS = 20
+NUM_COINS = 10
 NUM_ROUNDS = 3
-
-PlayerState = collections.namedtuple('PlayerState',
-                                     ['num_remaining_coins', 'num_rounds_won'])
 
 
 class State(
-    collections.namedtuple('State', ['player1', 'player2', 'num_rounds_tied'])):
+    collections.namedtuple('State', [
+        'num_coins_left_player1', 'num_coins_left_player2',
+        'delta_num_rounds_won', 'num_rounds_left'
+    ])):
   '''A state in the game.
 
   The state captures all the information necessary to play: the number of coins
@@ -37,75 +36,57 @@ class State(
   @staticmethod
   def initial_state():
     '''Creates the initial state of the game.'''
-    initial_player_state = PlayerState(
-        num_remaining_coins=NUM_COINS, num_rounds_won=0)
     return State(
-        player1=initial_player_state,
-        player2=initial_player_state,
-        num_rounds_tied=0)
+        num_coins_left_player1=NUM_COINS,
+        num_coins_left_player2=NUM_COINS,
+        delta_num_rounds_won=0,
+        num_rounds_left=NUM_ROUNDS)
 
   def __str__(self):
-    return '{: <2} ({}) | {: <2} ({}) | {}'.format(
-        self.player1.num_remaining_coins, self.player1.num_rounds_won,
-        self.player2.num_remaining_coins, self.player2.num_rounds_won,
-        self.num_rounds_tied)
+    return '{: <2} | {: <2} | {:+d}, {}'.format(
+        self.num_coins_left_player1, self.num_coins_left_player2,
+        self.delta_num_rounds_won, self.num_rounds_left)
 
-  def next_state(self, player1_coins_spent, player2_coins_spent):
+  def next_state(self, num_coins_spent_player1, num_coins_spent_player2):
     '''Returns the state of the game after both players play a round.'''
-    if self.player1.num_remaining_coins < player1_coins_spent:
+    if self.num_coins_left_player1 < num_coins_spent_player1:
       raise ValueError('Player 1 cannot spend {} coins in state {}'.format(
-          player1_coins_spent, self))
-    if self.player2.num_remaining_coins < player2_coins_spent:
+          num_coins_spent_player1, self))
+    if self.num_coins_left_player2 < num_coins_spent_player2:
       raise ValueError('Player 2 cannot spend {} coins in state {}'.format(
-          player2_coins_spent, self))
-
-    new_player1_state = PlayerState(
-        num_remaining_coins=self.player1.num_remaining_coins -
-        player1_coins_spent,
-        num_rounds_won=self.player1.num_rounds_won +
-        (player1_coins_spent > player2_coins_spent))
-
-    new_player2_state = PlayerState(
-        num_remaining_coins=self.player2.num_remaining_coins -
-        player2_coins_spent,
-        num_rounds_won=self.player2.num_rounds_won +
-        (player2_coins_spent > player1_coins_spent))
-
-    new_num_rounds_tied = self.num_rounds_tied + (
-        player1_coins_spent == player2_coins_spent)
+          num_coins_spent_player2, self))
 
     return State(
-        player1=new_player1_state,
-        player2=new_player2_state,
-        num_rounds_tied=new_num_rounds_tied)
-
-  def _num_remaining_rounds(self):
-    return NUM_ROUNDS - (
-        self.player1.num_rounds_won + self.player2.num_rounds_won +
-        self.num_rounds_tied)
+        num_coins_left_player1=self.num_coins_left_player1 -
+        num_coins_spent_player1,
+        num_coins_left_player2=self.num_coins_left_player2 -
+        num_coins_spent_player2,
+        delta_num_rounds_won=self.delta_num_rounds_won + int(
+            np.sign(num_coins_spent_player1 - num_coins_spent_player2)),
+        num_rounds_left=self.num_rounds_left - 1)
 
   def is_terminal_state(self):
     '''Returns true if the game is over.'''
-    return self._num_remaining_rounds() == 0
+    return self.num_rounds_left == 0
 
-  def _get_player_actions(self, player_state):
-    if (self._num_remaining_rounds() == 1):
-      return [player_state.num_remaining_coins]
-    return range(player_state.num_remaining_coins + 1)
+  def _get_player_actions(self, num_coins_left):
+    if self.num_rounds_left == 1:
+      return [num_coins_left]
+    return range(num_coins_left + 1)
 
-  def get_player1_actions(self):
+  def get_actions_player1(self):
     '''Returns the list of actions available to player 1.
 
     The order of the actions is guaranteed to be the same across multiple calls.
     '''
-    return self._get_player_actions(self.player1)
+    return self._get_player_actions(self.num_coins_left_player1)
 
-  def get_player2_actions(self):
-    '''Returns the list of actions available to player 1.
+  def get_actoins_player2(self):
+    '''Returns the list of actions available to player 2.
 
     The order of the actions is guaranteed to be the same across multiple calls.
     '''
-    return self._get_player_actions(self.player2)
+    return self._get_player_actions(self.num_coins_left_player2)
 
   def score(self):
     '''Returns the score of a terminal state for player 1.
@@ -114,7 +95,7 @@ class State(
     '''
     if not self.is_terminal_state():
       return ValueError('State {} is not terminal'.format(self))
-    return np.sign(self.player1.num_rounds_won - self.player2.num_rounds_won)
+    return np.sign(self.delta_num_rounds_won)
 
 
 def solve_matrix_game(matrix):
@@ -189,9 +170,9 @@ class StrategyBook(object):
     strategies_str = []
 
     def _sorting_order(state):
-      return (state.num_rounds_tied, -state.player1.num_remaining_coins,
-              -state.player2.num_remaining_coins, state.player1.num_rounds_won,
-              state.player2.num_rounds_won)
+      return (-state.num_rounds_left, np.abs(state.delta_num_rounds_won) +
+              state.delta_num_rounds_won / (NUM_ROUNDS + 1),
+              -state.num_coins_left_player1, -state.num_coins_left_player2)
 
     sorted_strategies = sorted(
         self._strategies.items(), key=lambda x: _sorting_order(x[0]))
@@ -200,7 +181,7 @@ class StrategyBook(object):
       if len(strategy) <= 1:
         continue
       strategy_str = str(state) + ': ['
-      actions = state.get_player1_actions()
+      actions = state.get_actions_player1()
       for action_ix, prob in enumerate(strategy):
         if action_ix != 0:
           strategy_str += ', '
@@ -223,8 +204,8 @@ def backwards_induction(state, strategy_book):
   if value:
     return value
 
-  player1_actions = state.get_player1_actions()
-  player2_actions = state.get_player2_actions()
+  player1_actions = state.get_actions_player1()
+  player2_actions = state.get_actoins_player2()
 
   matrix = np.zeros([len(player1_actions), len(player2_actions)])
 
